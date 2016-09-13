362406adb55ef0cab8c4f1d832ff8a67a7b6099c
DoomTay <bluejedi@cfl.rr.com>
2016-09-13 17:49:53 -0400

    Camera now follows player

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -13,8 +13,9 @@ public class Bird : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(Vector3.forward * -Input.GetAxis("Horizontal"));
-		print (Input.GetAxis ("Vertical"));
+		//print (Input.GetAxis ("Vertical"));
 		if (Input.GetAxis ("Vertical") > 0)
-			rb.AddForce (transform.up * 10 * Input.GetAxis ("Vertical"));
+			rb.AddForce (transform.up * 25 * Input.GetAxis ("Vertical"));
+		Camera.main.transform.position -= new Vector3 (((Camera.main.transform.position.x - transform.position.x) / 10), ((Camera.main.transform.position.y - transform.position.y) / 10), 0);
 	}
 }
