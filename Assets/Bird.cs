1a012f44c9d7f2bf747ae842d50c4990fe4c5659
DoomTay <bluejedi@cfl.rr.com>
2016-09-19 19:41:59 -0400

    Added death mechanic

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -4,18 +4,43 @@ using System.Collections;
 public class Bird : MonoBehaviour {
 
 	private Rigidbody2D rb;
+	public int lives = 5;
+	private Vector3 spawnPoint;
+	private bool alive = true;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		spawnPoint = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.forward * -Input.GetAxis("Horizontal"));
-		//print (Input.GetAxis ("Vertical"));
-		if (Input.GetAxis ("Vertical") > 0)
-			rb.AddForce (transform.up * 25 * Input.GetAxis ("Vertical"));
-		Camera.main.transform.position -= new Vector3 (((Camera.main.transform.position.x - transform.position.x) / 10), ((Camera.main.transform.position.y - transform.position.y) / 10), 0);
+		if (alive) {
+			transform.Rotate (Vector3.forward * -Input.GetAxis ("Horizontal"));
+			if (Input.GetAxis ("Vertical") > 0)
+				rb.AddForce (transform.up * 15 * Input.GetAxis ("Vertical"));
+			Camera.main.transform.position -= new Vector3 (((Camera.main.transform.position.x - transform.position.x) / 10), ((Camera.main.transform.position.y - transform.position.y) / 10), 0);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Ground")
+			StartCoroutine ("Death");
+	}
+
+	IEnumerator Death ()
+	{
+		alive = false;
+		GetComponent<Renderer> ().enabled = false;
+		yield return new WaitForSeconds(5f);
+		if (lives > 0) {
+			lives--;
+			transform.position = spawnPoint;
+			transform.rotation = Quaternion.identity;
+			GetComponent<Renderer> ().enabled = true;
+			alive = true;
+		} else
+			print ("Game Over");
 	}
 }
